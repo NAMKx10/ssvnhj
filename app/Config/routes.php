68e29f6b7e1d9f3d3748fa535eab9b551140a8b4
NAMKx10 <najix10@gmail.com>
2025-09-23 20:50:35 +0300

    Update routes.php

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -32,6 +32,26 @@ return [
     'handle_role_edit_details' => 'Common/Controllers/RoleHandlerController.php',
     'handle_role_delete' => 'Common/Controllers/RoleHandlerController.php',
 
+    // --- موديل الفروع ---
+    'branches' => 'Modules/Branches/Controllers/BranchesController.php',
+    'branches/add' => 'Modules/Branches/Views/add_view.php',
+    'branches/edit' => 'Modules/Branches/Views/edit_view.php',
+    'branches/batch_add' => 'Modules/Branches/Controllers/BranchesBatchController.php',
+    'branches/batch_edit' => 'Modules/Branches/Controllers/BranchesBatchController.php',
+    'handle_branch_actions' => 'Modules/Branches/Controllers/BranchHandlerController.php',
+
+        // --- موديل التقارير ---
+    'reports' => 'Modules/Reports/Controllers/ReportsController.php',
+    'settings/print_templates' => 'Common/Controllers/PrintTemplatesController.php',
+    'handle_print_template_update' => 'Common/Controllers/PrintTemplateHandlerController.php',
+
+        // --- موديل الوثائق والمستندات ---
+    'documents' => 'Modules/Documents/Controllers/DocumentsController.php',
+    'documents/add' => 'Modules/Documents/Views/add_view.php',
+    'documents/edit' => 'Modules/Documents/Views/edit_view.php',
+    'handle_document_actions' => 'Modules/Documents/Controllers/DocumentHandlerController.php',
+    'ajax_get_targets' => 'Modules/Documents/Controllers/AjaxController.php',
+
     'permissions' => 'Common/Controllers/PermissionsController.php',
     'permissions/add_group' => 'Common/Views/permissions/add_group_view.php',
     'permissions/edit_group' => 'Common/Views/permissions/edit_group_view.php',
@@ -39,8 +59,32 @@ return [
     'permissions/edit' => 'Common/Views/permissions/edit_permission_view.php',
     'handle_permission_actions' => 'Common/Controllers/PermissionHandlerController.php',
 
-    
-    // سنضيف بقية المسارات هنا لاحقًا
-    // 'users' => 'Modules/Users/Controllers/UserController.php',
-    // 'users/create' => 'Modules/Users/Controllers/UserCreateController.php',
+        // --- موديل جهات الاتصال ---
+    'contacts' => 'Modules/Contacts/Controllers/ContactsController.php',
+    'contacts/add' => 'Modules/Contacts/Views/add_view.php',
+    'contacts/edit' => 'Modules/Contacts/Views/edit_view.php',
+    'contacts/batch_add' => 'Modules/Contacts/Controllers/ContactsBatchController.php',
+    'contacts/batch_edit' => 'Modules/Contacts/Controllers/ContactsBatchController.php',
+    'handle_contact_actions' => 'Modules/Contacts/Controllers/ContactHandlerController.php',
+
+        // --- موديل تهيئة المدخلات ---
+    'settings' => 'Common/Controllers/SettingsController.php',
+    'settings/add_group' => 'Common/Views/settings/add_group_view.php',
+    'settings/edit_group' => 'Common/Views/settings/edit_group_view.php',
+    'settings/add_option' => 'Common/Views/settings/add_option_view.php',
+    'settings/edit_option' => 'Common/Views/settings/edit_option_view.php',
+    'handle_settings_actions' => 'Common/Controllers/SettingsHandlerController.php',
+
+        // --- مدير الملفات ---
+    'file-manager' => 'Modules/FileManager/Controllers/FileManagerController.php',
+    'file-manager/add_folder' => 'Modules/FileManager/Views/add_folder_view.php',
+    'file-manager/upload_file' => 'Modules/FileManager/Views/upload_file_view.php',
+    'file-manager/rename' => 'Modules/FileManager/Views/rename_view.php',
+    'file-manager/move' => 'Modules/FileManager/Views/move_view.php',
+    'handle_file_actions' => 'Modules/FileManager/Controllers/FileActionsHandler.php',
+
+        // --- إعدادات النظام العامة ---
+    'settings/general' => 'Common/Controllers/GeneralSettingsController.php',
+    'handle_general_settings_update' => 'Common/Controllers/GeneralSettingsHandlerController.php',
+
 ];
